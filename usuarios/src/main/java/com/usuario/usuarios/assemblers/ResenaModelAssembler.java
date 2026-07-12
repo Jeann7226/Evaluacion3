2fc2460e01577f632ce5f141a416e1b158a9d4f2
RappCL <ra.palestro@duocuc.cl>
2026-07-11 21:25:50 -0400

    Assemblers

--- usuarios/src/main/java/com/usuario/usuarios/assemblers/ResenaModelAssembler.java
+++ usuarios/src/main/java/com/usuario/usuarios/assemblers/ResenaModelAssembler.java
@@ -1,5 +1,19 @@
 package com.usuario.usuarios.assemblers;
 
-public class ResenaModelAssembler {
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+
+import com.usuario.usuarios.controller.ResenaController;
+import com.usuario.usuarios.model.Resena;
+
+public class ResenaModelAssembler implements RepresentationModelAssembler<Resena, EntityModel<Resena>>{
+    @Override
+    public EntityModel<Resena> toModel(Resena resena) {
+        return EntityModel.of(resena,
+            linkTo(methodOn(ResenaController.class).buscarResena(resena.getId_resena())).withSelfRel()
+        );
+    }
 }
--- usuarios/src/main/java/com/usuario/usuarios/assemblers/UsuarioModelAssembler.java
+++ usuarios/src/main/java/com/usuario/usuarios/assemblers/UsuarioModelAssembler.java
@@ -1,5 +1,19 @@
 package com.usuario.usuarios.assemblers;
 
-public class UsuarioModelAssembler {
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+
+import com.usuario.usuarios.controller.UsuarioController;
+import com.usuario.usuarios.model.Usuario;
+
+public class UsuarioModelAssembler implements RepresentationModelAssembler<Usuario, EntityModel<Usuario>>{
+    @Override
+    public EntityModel<Usuario> toModel(Usuario usuario) {
+        return EntityModel.of(usuario,
+            linkTo(methodOn(UsuarioController.class).buscarUsuario(usuario.getId_usuario())).withSelfRel()
+        );
+    }
 }
